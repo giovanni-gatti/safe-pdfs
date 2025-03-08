e8ea4a76d904084e33229a27a95eb6ab4826e424
giovannig <giovanni.gatti01@gmail.com>
2025-03-08 18:38:30 +0100

    added ner for products

--- parser/model/anonymize.py
+++ parser/model/anonymize.py
@@ -24,8 +24,8 @@ class Anonymizer:
         # Convert the extracted documents to Markdown format
         md_docs = [doc.export_to_markdown() for doc in documents]
         for i, doc in tqdm(enumerate(nlp.pipe(md_docs, disable=["tagger", "parser", "attribute_ruler", "lemmatizer"])), desc=f"Anonymizing documents", total=len(md_docs)):
-            # Anonymize named entities referring to people and organizations in the documents by replacing them with their entity type
-            anonymized_doc = "".join([f"[{t.ent_type_}]"+t.whitespace_ if t.ent_type_ in ["ORG", "PERSON"] else t.text_with_ws for t in doc])
+            # Anonymize named entities referring to people, products and organizations in the documents by replacing them with their entity type
+            anonymized_doc = "".join([f"[{t.ent_type_}]"+t.whitespace_ if t.ent_type_ in ["ORG", "PERSON", "PRODUCT"] else t.text_with_ws for t in doc])
             # Save the anonymized documents to the output folder
             output_file = output_folder / f"{doc_names[i]}.md"
             with open(output_file, "w") as f:
